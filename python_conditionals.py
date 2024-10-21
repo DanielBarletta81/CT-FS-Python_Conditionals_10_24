b9abfc972b627cb505d726863e473a555efd4d55
DanielBarletta81 <dbarcelona1481@gmail.com>
2024-10-20 21:16:56 -0400

    add task 3 leap year checker.

--- python_conditionals.py
+++ python_conditionals.py
@@ -59,6 +59,21 @@ else:
 
 
 
+#Task 3:
+#  Leap Year Checker Write a Python program that prompts the user to input a year. 
+# The program should determine if the given year is a leap year or not and then display an appropriate message. Please note that this is the definition of a leap year: Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible by 100, but these centurial years are leap years if they are exactly divisible by 400.
+
+#Expected Outcome: If you test the year 1900, is should be False. The year 2000 should be True. 
+# The year 2024 should be True
+
+year_chosen = int(input("Please enter a year: "))
+
+if (year_chosen % 4 == 0) and not (year_chosen % 100 == 0):
+    print("The year you entered is a leap year.")
+elif(year_chosen % 400 == 0) and (year_chosen % 100 == 0):
+    print("The year entered is a leap year.")
+else:
+    print("The year entered is not a leap year.") 
 
 
 
